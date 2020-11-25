10a5d4f40d33b6fdd81439895bf90681a90af738
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-11-25 10:46:58 +0000

    finito visualizzare titolo, trama, locandina

--- mflix-visualizer/src/app/app.component.ts
+++ mflix-visualizer/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +13,22 @@ export class AppComponent {
   results : Object[];
   obs : Observable<Object>;
 
-  constructor (private http : HttpClient) {};
+  constructor (private http : HttpClient, private sanitazer : DomSanitizer) {};  //iniettare l’oggetto DOM Sanitizer nel costruttore (importa DomSanitizer)
 
   //metodo per richiedere al server la lista dei 10 film
   load10Movies()
   {
-    this.obs = this.http.get('https://3000-e46ccc48-87c1-4bc9-b9ee-b2f348ef9471.ws-eu01.gitpod.io/users');
+    this.obs = this.http.get('https://3000-b9fecb66-73fa-4ece-9fa1-da944f356b37.ws-eu01.gitpod.io/users');
     this.obs.subscribe(this.getData);
   }
   getData = (data) => {
     this.results = data;
+  }
+
+  /*aggiunto metodo per sanificare il codice.
+  L'oggetto sanitize, "sanifica url passato come parametro*/
+  photoURL(urltoSanitize) {
+    console.log(urltoSanitize);
+    return this.sanitazer.bypassSecurityTrustUrl(urltoSanitize);
   }
 }
